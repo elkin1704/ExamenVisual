876e680eaf1e422602f727dba7fee261dc9d8c65
elkin1704 <60784779+elkin1704@users.noreply.github.com>
2022-01-24 10:22:14 -0500

    Update Program.cs

--- Examen/Consola/Program.cs
+++ Examen/Consola/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+usin ModeloDb;
 namespace Consola
 {
     internal class Program
@@ -7,7 +7,18 @@ namespace Consola
         static void Main(string[] args)
         {
            
-            
+             Category act = new Category()
+            {
+                Name = "El conjuro",
+                Last_Update = "24/01/2022"
+                
+            };
+            // Grabar los datos
+            using (DbExamen repos = new DbExamen())
+            {
+                repos.ResumenAmortizacions.AddRange(act);
+                repos.SaveChanges();
+            }
         }
     
     }
