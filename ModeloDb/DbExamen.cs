04d4c61dc2dba6e34e0321866b9a17a79d7653ec
elkin1704 <60784779+elkin1704@users.noreply.github.com>
2022-01-25 08:39:56 -0500

    Add files via upload

---
+++ ModeloDb/DbExamen.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Entidades;
+
+namespace ModeloDb
+{
+    public class DbExamen : DbContext
+    {
+        public DbSet<Film> Films    { get; set; }
+        public DbSet<Film_Category> Film_Categorys { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Film_Actor> Film_Actors { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            String connSQL = "server = DESKTOP-8BBLG0F\\SQLEXPRESS ; database=Examen; user id=sa; password=1234";
+            optionsBuilder.UseSqlServer(connSQL);
+        }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            // Films_Actor
+            model.Entity<Film_Actor>()
+                  .HasOne(film_actor => film_actor.Actors)
+                  .WithMany(actor => actor.Film_Actors)
+                  .HasForeignKey(fil => fil.ActorId);
+            model.Entity<Film_Actor>()
+                  .HasOne(film_actor => film_actor.Films)
+                  .WithMany(film => film.Film_Actors)
+                  .HasForeignKey(fil => fil.FilmId);
+
+            // 
+
+            model.Entity<Film_Actor>()
+                .HasKey(film_actor => new
+                {
+                    film_actor.ActorId,
+                    film_actor.FilmId
+                });
+
+            model.Entity<Film_Category>()
+                  .HasOne(film_category => film_category.Categorys)
+                  .WithMany(category => category.Film_Categorys)
+                  .HasForeignKey(fil => fil.CategoryId);
+            model.Entity<Film_Category>()
+                  .HasOne(film_category => film_category.Films)
+                  .WithMany(film => film.Film_Categorys)
+                  .HasForeignKey(fil => fil.FilmId);
+
+            // 
+
+            model.Entity<Film_Category>()
+                .HasKey(film_category => new
+                {
+                    film_category.CategoryId,
+                    film_category.FilmId
+                });
+
+
+
+        }
+    }
+}
---
+++ ModeloDb/Migrations/20220125022858_cargafinal.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModeloDb.Migrations
+{
+    public partial class cargafinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    ActorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Last_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Last_Update = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.ActorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categorys",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Last_Update = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorys", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    FilmId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Release_Year = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LenguageID = table.Column<int>(type: "int", nullable: false),
+                    Rental_Duration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rental_Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Length = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Replacement_Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Rating = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Last_Update = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Special_Features = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fulltext = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.FilmId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Film_Actors",
+                columns: table => new
+                {
+                    FilmId = table.Column<int>(type: "int", nullable: false),
+                    ActorId = table.Column<int>(type: "int", nullable: false),
+                    Last_Update = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film_Actors", x => new { x.ActorId, x.FilmId });
+                    table.ForeignKey(
+                        name: "FK_Film_Actors_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "ActorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Film_Actors_Films_FilmId",
+                        column: x => x.FilmId,
+                        principalTable: "Films",
+                        principalColumn: "FilmId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Film_Categorys",
+                columns: table => new
+                {
+                    FilmId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Last_Update = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Film_Categorys", x => new { x.CategoryId, x.FilmId });
+                    table.ForeignKey(
+                        name: "FK_Film_Categorys_Categorys_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categorys",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Film_Categorys_Films_FilmId",
+                        column: x => x.FilmId,
+                        principalTable: "Films",
+                        principalColumn: "FilmId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Film_Actors_FilmId",
+                table: "Film_Actors",
+                column: "FilmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Film_Categorys_FilmId",
+                table: "Film_Categorys",
+                column: "FilmId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Film_Actors");
+
+            migrationBuilder.DropTable(
+                name: "Film_Categorys");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Categorys");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
